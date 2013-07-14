5218eba2b9c8f5110229ac6b8935bf6e06b2c2fd
dtan4 <dtan4@mac>
2013-07-15 02:57:14 +0900

    Disable auto-indent in markdown mode

--- inits/60-markdown.el
+++ inits/60-markdown.el
@@ -4,3 +4,5 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(setq markdown-indent-on-enter nil)
