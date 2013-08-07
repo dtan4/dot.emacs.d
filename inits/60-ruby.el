6612122b406c5774a9c43663345726e5f1a6dbdf
dtan4 <dtan4@mac>
2013-08-08 02:11:52 +0900

    Enable flymake on ruby-mode

--- inits/60-ruby.el
+++ inits/60-ruby.el
@@ -9,11 +9,11 @@
 ;; ruby-mode extension
 (require 'ruby-end)
 (add-hook 'ruby-mode-hook
-  '(lambda ()
-    (abbrev-mode 1)
-    (electric-pair-mode t)
-    (electric-indent-mode t)
-    (electric-layout-mode t)))
+          '(lambda ()
+             (abbrev-mode 1)
+             (electric-pair-mode t)
+             (electric-indent-mode t)
+             (electric-layout-mode t)))
 
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
@@ -28,3 +28,43 @@
 
 ;; RSpec-mode
 (require 'rspec-mode)
+
+;; flymake on ruby-mode
+;; https://github.com/sutetotanuki/emacs/blob/master/config/packages/ruby-mode.el
+(require 'flymake)
+;; Invoke ruby with '-c’ to get syntax checking
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "ruby" (list "-c" local-file))))
+
+(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+
+(add-hook
+ 'ruby-mode-hook
+ '(lambda ()
+    (if (not (null buffer-file-name)) (flymake-mode))
+    (define-key ruby-mode-map "\C-cd" 'credmp/flymake-display-err-minibuf)))
+
+(defun credmp/flymake-display-err-minibuf ()
+  "Displays the error/warning for the current line in the minibuffer"
+  (interactive)
+  (let* ((line-no (flymake-current-line-no))
+         (line-err-info-list (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+         (count (length line-err-info-list))
+         )
+    (while (> count 0)
+      (when line-err-info-list
+        (let* ((file (flymake-ler-file (nth (1- count) line-err-info-list)))
+               (full-file (flymake-ler-full-file (nth (1- count) line-err-info-list)))
+               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
+               (line (flymake-ler-line (nth (1- count) line-err-info-list))))
+          (message "[%s] %s" line text)
+          )
+        )
+      (setq count (1- count)))))
