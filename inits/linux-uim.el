df724aee1b6fc2edc3e3b97cf2b614c7e78cc164
dtan4 <dtanshi45@gmail.com>
2013-08-20 08:56:42 +0900

    Bind M-` to toggle uim

--- inits/linux-uim.el
+++ inits/linux-uim.el
@@ -1,13 +1,15 @@
 (require 'uim)
-(uim-mode)
 (setq uim-lang-code-alist
       (cons '("Japanese" "Japanese" utf-8 "UTF-8")
             (delete (assoc "Japanese" uim-lang-code-alist)
                     uim-lang-code-alist)))
 (global-set-key [zenkaku-hankaku] 'uim-mode)
 (global-set-key (kbd "s-`") 'uim-mode)
+(global-set-key (kbd "M-`") 'uim-mode)
 (add-hook 'uim-load-hook
           '(lambda ()
              (define-key uim-mode-map [67108896] nil)
              (define-key uim-mode-map [0] nil)))
 (setq uim-candidate-display-inline t)
+
+(uim-mode-on)
