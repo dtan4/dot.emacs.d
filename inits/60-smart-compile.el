38134a18571a0cb71b7c3742969f0d467aaf9b4d
dtan4 <dtan4@mac>
2013-08-22 17:15:53 +0900

    Scroll automatically in compilation buffer

--- inits/60-smart-compile.el
+++ inits/60-smart-compile.el
@@ -4,3 +4,4 @@
 (define-key ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
 (global-set-key (kbd "C-c x") 'kill-compilation)
 (setq compilation-window-height 15)
+(setq compilation-scroll-output t)
