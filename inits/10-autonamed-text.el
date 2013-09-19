a3a46cce24508849cbfb84d25de7fe57b8f72087
dtan4 <dtanshi45@gmail.com>
2013-09-19 13:14:28 +0900

    Remove computer name from filename of autonamed-txt

--- inits/10-autonamed-text.el
+++ inits/10-autonamed-text.el
@@ -1,6 +1,6 @@
 (defun my-autonamed-txt()
   (interactive)
   (find-file
-   (concat "~/Dropbox/memo/daily/" (format-time-string "%Y-%m-%d_") (system-name) ".md"))
+   (concat "~/Dropbox/memo/daily/" (format-time-string "%Y-%m-%d") ".md"))
   )
 (global-set-key "\C-cf" 'my-autonamed-txt)
