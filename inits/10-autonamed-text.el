be381174f4a03fcfd77c857e2a522d90981600dd
dtan4 <dtanshi45@gmail.com>
2013-09-19 13:11:11 +0900

    Change save directory of autonamed-txt

--- inits/10-autonamed-text.el
+++ inits/10-autonamed-text.el
@@ -1,6 +1,6 @@
 (defun my-autonamed-txt()
   (interactive)
   (find-file
-   (concat "~/git/dtan4-memo/" (format-time-string "%Y-%m-%d_") (system-name) ".md"))
+   (concat "~/Dropbox/memo/daily/" (format-time-string "%Y-%m-%d_") (system-name) ".md"))
   )
 (global-set-key "\C-cf" 'my-autonamed-txt)
