b2f2f0196e3b0e6e64dcce08940efc9d8c3373e5
dtan4 <dtanshi45@gmail.com>
2013-10-07 13:26:49 +0900

    Remove description about rinari

--- inits/60-ruby.el
+++ inits/60-ruby.el
@@ -24,8 +24,6 @@
 
 ;; eRuby (rhtml-mode)
 (require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-          (lambda () (rinari-launch)))
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 
 ;; RSpec-mode
