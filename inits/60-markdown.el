99bf978c6b8f6970e11e6bd5c44e239df3f954ea
dtan4 <dtanshi45@gmail.com>
2013-10-24 11:12:57 +0900

    Fix to load gfm-mode rightly

--- inits/60-markdown.el
+++ inits/60-markdown.el
@@ -1,6 +1,4 @@
 ;; markdown-mode
-(autoload 'gfm-mode "gfm-mode"
-  "Major mode for editing Github Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
