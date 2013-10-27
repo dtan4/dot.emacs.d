9a4347cb1c8b5be5f887a9d90814fa89ef946289
dtan4 <dtanshi45@gmail.com>
2013-10-27 18:04:49 +0000

    Separate Linux's font settings

--- inits/linux-appearance.el
+++ inits/linux-appearance.el
@@ -1,12 +1,3 @@
-;; Font settings
-;; 01234567890123456789
-;; あいうえおかきくけこ
-(set-face-attribute 'default nil
-                    :family "Ricty"
-                    :height 120)
-(set-fontset-font nil 'japanese-jisx0208
-                  (font-spec :family "Ricty" :registry "unicode-bmp"))
-
 (setq initial-frame-alist
       (append (list
                '(width . 80)
