f076d0a3c1ec92861749b647e280b1dd70744140
dtan4 <dtanshi45@gmail.com>
2013-12-10 15:15:54 +0900

    Execute smart-compile from enh-ruby-mode

--- inits/61-smart-compile.el
+++ inits/61-smart-compile.el
@@ -2,6 +2,8 @@
 (require 'smart-compile)
 (define-key ruby-mode-map (kbd "C-c c") 'smart-compile)
 (define-key ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
+(define-key enh-ruby-mode-map (kbd "C-c c") 'smart-compile)
+(define-key enh-ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
 (global-set-key (kbd "C-c x") 'kill-compilation)
 (setq compilation-window-height 15)
 (setq compilation-scroll-output t)
