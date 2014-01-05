78e4978076d9d6c50d51043a8eb353a06c4193e4
dtan4 <dtanshi45@gmail.com>
2014-01-05 18:41:07 +0900

    Bind 'C-c b' to execute magit-blame-mode

--- inits/50-magit.el
+++ inits/50-magit.el
@@ -2,6 +2,7 @@
 (require 'magit)
 (add-hook 'magit-mode-hook 'ansi-color-for-comint-mode-on)
 (global-set-key (kbd "C-c g") 'magit-status)
+(global-set-key (kbd "C-c b") 'magit-blame-mode)
 
 (set-face-foreground 'magit-diff-add "#b9ca4a")
 (set-face-foreground 'magit-diff-del "#d54e53")
