cc413f820a35364bc56ec65e222934758bbbe88d
dtan4 <dtanshi45@gmail.com>
2014-01-10 01:59:32 +0900

    Show YaTeX buffer in popwin

--- inits/30-popwin.el
+++ inits/30-popwin.el
@@ -1,3 +1,11 @@
 ;; popwin.el
 (require 'popwin)
+
+(defadvice YaTeX-showup-buffer (around popwin-yatex:YaTeX-showup-buffer (buffer &optional func select) activate)
+  (popwin:display-buffer-1 buffer
+                           :default-config-keywords `(:noselect ,(not select))
+                           :if-config-not-found (lambda (buffer) ad-do-it)))
+(push '("*YaTeX-typesetting*") popwin:special-display-config)
+(push '("*dvi-preview*") popwin:special-display-config)
+
 (setq display-buffer-function 'popwin:display-buffer)
