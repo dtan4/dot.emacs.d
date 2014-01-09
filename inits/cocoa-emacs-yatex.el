57e494d055e348ddb33c98e076d3727bd9057eff
dtan4 <dtanshi45@gmail.com>
2014-01-10 01:46:31 +0900

    Use platex as tex command

--- inits/cocoa-emacs-yatex.el
+++ inits/cocoa-emacs-yatex.el
@@ -1,7 +1,7 @@
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
 (setq auto-mode-alist
       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(setq tex-command "~/Library/TeXShop/bin/platex2pdf-utf8"
+(setq tex-command "platex"
       dvi2-command "open -a TeXShop")
 (setq YaTeX-kanji-code 4)
 (add-hook 'yatex-mode-hook
