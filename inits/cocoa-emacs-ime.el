1baaa720a5cdce627f38bc451480aac8e22c9a8d
dtan4 <dtanshi45@gmail.com>
2014-01-12 00:16:15 +0900

    Force us-mode in minibuffer

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,2 +1,3 @@
 (setq default-input-method "MacOSX")
 (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
+(add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
