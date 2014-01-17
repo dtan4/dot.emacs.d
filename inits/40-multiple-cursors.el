c0c9f261254eb378ca61c74c6c975025b95a566b
dtan4 <dtanshi45@gmail.com>
2014-01-17 19:09:01 +0900

    Change keybind of smartrep with multiple-cursors

--- inits/40-multiple-cursors.el
+++ inits/40-multiple-cursors.el
@@ -3,6 +3,8 @@
 
 (global-set-key (kbd "C-M-c") 'mc/edit-lines)
 (smartrep-define-key
- global-map "C-." '(("C-n" . 'mc/mark-next-like-this)
-                    ("C-p" . 'mc/mark-previous-like-this)
-                    ("*"   . 'mc/mark-all-like-this)))
+ global-map "C-." '(("n" . 'mc/mark-next-like-this)
+                    ("p" . 'mc/mark-previous-like-this)
+                    ("s" . 'mc/skip-to-next-like-this)
+                    ("S" . 'mc/skip-to-previous-like-this)
+                    ("*" . 'mc/mark-all-like-this)))
