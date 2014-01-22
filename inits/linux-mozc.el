0332ef3df0b67e914d7b2544749a2ae8ff9ed876
dtan4 <dtanshi45@gmail.com>
2014-01-22 16:59:15 +0900

    Fix default encoding in Linux

--- inits/linux-mozc.el
+++ inits/linux-mozc.el
@@ -1,5 +1,4 @@
 (load-file "/usr/share/emacs/site-lisp/emacs-mozc/mozc.el")
-(set-language-environment "Japanese")
 (setq default-input-method "japanese-mozc")
 (setq mozc-candidate-style 'overlay)
 (global-set-key (kbd "s-SPC") 'toggle-input-method)
