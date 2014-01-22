cae112f2692c0cddfd2e4a736b1aedd5e7438bb9
dtan4 <dtanshi45@gmail.com>
2014-01-22 14:10:51 +0900

    Change cursor color by IME mode in Linux

--- inits/linux-mozc.el
+++ inits/linux-mozc.el
@@ -5,3 +5,7 @@
 (global-set-key (kbd "s-SPC") 'toggle-input-method)
 (global-set-key (kbd "s-`") 'toggle-input-method)
 (global-set-key (kbd "M-`") 'toggle-input-method)
+
+;; http://d.hatena.ne.jp/kitokitoki/20110815/p4
+(add-hook 'input-method-activate-hook '(lambda () (set-cursor-color "red")))
+(add-hook 'input-method-inactivate-hook '(lambda () (set-cursor-color "yellow")))
