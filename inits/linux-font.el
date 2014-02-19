36c9adcfd69d8c76aa9a48c191ec2b6abd6416bd
dtan4 <dtanshi45@gmail.com>
2014-02-19 18:53:44 +0000

    Track linux-font.el

--- inits/linux-font.el
+++ inits/linux-font.el
@@ -1,5 +1,5 @@
 (set-face-attribute 'default nil
                     :family "Ricty"
-                    :height 160)
+                    :height 130)
 (set-fontset-font nil 'japanese-jisx0208
                   (font-spec :family "Ricty" :registry "unicode-bmp"))
