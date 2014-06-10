6ce152e871946c56c49c94efaf9c9c73ab945859
dtan4 <dtanshi45@gmail.com>
2014-06-11 02:28:48 +0900

    Start completion automatically

--- inits/40-auto-complete.el
+++ inits/40-auto-complete.el
@@ -2,7 +2,7 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
-(setq ac-auto-start nil)
+(setq ac-auto-start 4)
 (setq ac-use-menu-map t)
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
