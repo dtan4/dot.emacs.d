a2b89755a76d1b4acbfa584aea5ab2d402dc2432
dtan4 <dtanshi45@gmail.com>
2014-06-14 05:41:17 +0900

    Move elisps out of package.el to site-lisp directory

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
 (package-initialize)
 
 ;; load all elisps under ~/.emacs.d/elisp
-(let ((default-directory (concat user-emacs-directory "elisp")))
+(let ((default-directory (concat user-emacs-directory "site-lisp")))
   (add-to-list 'load-path default-directory)
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
