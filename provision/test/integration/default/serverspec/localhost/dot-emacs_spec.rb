f991f81a3752ef7e9ad079e07394bb1630569d40
dtan4 <dtanshi45@gmail.com>
2014-06-13 19:02:32 +0900

    Modify dot-emacs_spec.rb

--- provision/test/integration/default/serverspec/localhost/dot-emacs_spec.rb
+++ provision/test/integration/default/serverspec/localhost/dot-emacs_spec.rb
@@ -2,10 +2,10 @@ require "serverspec"
 include Serverspec::Helper::DetectOS
 include Serverspec::Helper::Exec
 
-package("git") do
+describe package("git") do
   it { should be_installed }
 end
 
-file("/home/vagrant/dot.emacs.d") do
+describe file("/home/vagrant/dot.emacs.d") do
   it { should be_directory }
 end
