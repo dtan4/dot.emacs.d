e5ef49ba38d0bc984286a3e162c7df9255ad03eb
dtan4 <dtanshi45@gmail.com>
2014-06-14 05:43:09 +0900

    Define auto-insert-alist from scratch

--- inits/50-autoinsert.el
+++ inits/50-autoinsert.el
@@ -3,16 +3,16 @@
 (setq auto-insert-directory (concat user-emacs-directory "template"))
 ;; 各ファイルによってテンプレートを切り替える
 (setq auto-insert-alist
-      (nconc '(
-               ("\\.cpp$" . ["template.cpp" my-template])
-               ("\\.c$" . ["template.c" my-template])
-               ("\\.tex$" . ["template.tex" my-template])
-               ("\\.java$" . ["template.java" my-template])
-               ("_spec\\.rb$" . ["template_spec.rb" my-template])
-               ("\\.rb$" . ["template.rb" my-template])
-               ("\\.pl$" . ["template.pl" my-template])
-               ("\\.sh$" . ["template.sh" my-template])
-               ) auto-insert-alist))
+      '(
+        ("\\.cpp$" . ["template.cpp" my-template])
+        ("\\.c$" . ["template.c" my-template])
+        ("\\.tex$" . ["template.tex" my-template])
+        ("\\.java$" . ["template.java" my-template])
+        ("_spec\\.rb$" . ["template_spec.rb" my-template])
+        ("\\.rb$" . ["template.rb" my-template])
+        ("\\.pl$" . ["template.pl" my-template])
+        ("\\.sh$" . ["template.sh" my-template])
+        ))
 (require 'cl)
 (defvar template-replacements-alists
   '(("%file%"             . (lambda () (file-name-nondirectory (buffer-file-name))))
