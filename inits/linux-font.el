d8640e5fef867729bb8b3bd866457febdbb38df9
dtan4 <dtanshi45@gmail.com>
2014-06-14 11:09:30 +0900

    Set font in Linux only if the specified font is available

--- inits/linux-font.el
+++ inits/linux-font.el
@@ -1,5 +1,6 @@
-(set-face-attribute 'default nil
-                    :family "Ricty"
-                    :height 130)
-(set-fontset-font nil 'japanese-jisx0208
-                  (font-spec :family "Ricty" :registry "unicode-bmp"))
+(if (find-font (font-spec :name "Ricty"))
+    (set-face-attribute 'default nil
+                        :family "Ricty"
+                        :height 130))
+;; (set-fontset-font nil 'japanese-jisx0208
+;;                   (font-spec :family "Ricty" :registry "unicode-bmp"))
