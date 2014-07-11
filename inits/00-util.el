7d932008b37c575a9830b8ace794a1b7aecc3d35
dtan4 <dtanshi45@gmail.com>
2014-07-12 01:04:33 +0900

    Divide into 3 windows

--- inits/00-util.el
+++ inits/00-util.el
@@ -94,3 +94,28 @@
 ;; http://ymotongpoo.hatenablog.com/entry/20110206/1296983970
 (setq vc-follow-symlinks t)
 (setq auto-revert-check-vc-info t)
+
+;; Divide into 3 windows
+;; http://d.hatena.ne.jp/yascentur/20110621/1308585547
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+(global-set-key "\C-x@" '(lambda ()
+                           (interactive)
+                           (split-window-vertically-n 3)))
+(global-set-key "\C-x#" '(lambda ()
+                           (interactive)
+                           (split-window-horizontally-n 3)))
