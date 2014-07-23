245dd9445fe1eef475f230aff451d9bde5c2fb5c
dtan4 <dtanshi45@gmail.com>
2014-07-23 23:47:35 +0900

    Remove provision directory

--- provision/test/integration/default/serverspec/localhost/emacs_spec.rb
+++
@@ -1,9 +0,0 @@
-require "serverspec"
-include Serverspec::Helper::DetectOS
-include Serverspec::Helper::Exec
-
-%w(emacs24 emacs24-el texinfo).each do |pkg|
-  describe package(pkg) do
-    it { should be_installed }
-  end
-end
