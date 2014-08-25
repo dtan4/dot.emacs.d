947efe9df97cead9025f9c7a1283891debf0d2aa
dtan4 <dtanshi45@gmail.com>
2014-08-25 14:43:53 +0900

    Display Simplified Chinese characters with the appropriate font

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -1,6 +1,7 @@
 ;; Font settings
 ;; 01234567890123456789
 ;; あいうえおかきくけこ
+;; 开发者中心
 (if (find-font (font-spec :name "Ricty"))
     (progn (set-face-attribute 'default nil
                         :family "Ricty"
@@ -8,12 +9,9 @@
            (set-fontset-font nil 'japanese-jisx0208
                              (font-spec :family "Ricty" :registry "unicode-bmp"))))
 
-;; (setq initial-frame-alist
-;;      (append (list
-;;               '(width . 90)
-;;               '(height . 60)
-;;               )
-;;              initial-frame-alist))
+(set-fontset-font t 'han (font-spec :family "Hei" :height 160))
+
+;; Required to show green characters
 (setq default-frame-alist initial-frame-alist)
 
 (set-scroll-bar-mode nil)
