3eb8ef13157e928f23fd237f797618303bdbfeec
dtan4 <dtanshi45@gmail.com>
2014-09-05 12:21:06 +0900

    Enable godef-jump

--- inits/60-go.el
+++ inits/60-go.el
@@ -3,3 +3,5 @@
      (require 'go-autocomplete)))
 
 (add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)))
