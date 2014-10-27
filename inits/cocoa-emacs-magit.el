af3d5b33765c3f01f73e86e3c8e6ac01b38fc07d
Daisuke FUJITA <dtanshi45@gmail.com>
2014-10-28 02:56:02 +0900

    Upgrade to Emacs 24.4

--- inits/cocoa-emacs-magit.el
+++ inits/cocoa-emacs-magit.el
@@ -1,1 +1,1 @@
-(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.4/bin/emacsclient")
