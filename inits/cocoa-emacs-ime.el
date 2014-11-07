36bef14262d1ee0c915822dc173933ccb684c709
Daisuke FUJITA <dtanshi45@gmail.com>
2014-11-07 18:14:10 +0900

    Disable Mac IME settings
    inline-patch is not compatible with Emacs 24.4

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,7 +1,9 @@
-(setq default-input-method "MacOSX")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "red")
-(mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" `cursor-color "yellow")
-(add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
-(add-hook 'isearch-mode-hook 'mac-change-language-to-us)
-(mac-translate-from-yen-to-backslash)
+;; (14-11-07) inline-patch is not compatible with Emacs 24.4!!!!!
+
+;; (setq default-input-method "MacOSX")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "red")
+;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" `cursor-color "yellow")
+;; (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
+;; (add-hook 'isearch-mode-hook 'mac-change-language-to-us)
+;; (mac-translate-from-yen-to-backslash)
