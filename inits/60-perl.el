a287f2667dc269210285c3b54eec6a725ed44f68
Daisuke Fujita <dtanshi45@gmail.com>
2014-12-23 02:00:42 +0900

    Open some Perl files with cperl-mode

--- inits/60-perl.el
+++ inits/60-perl.el
@@ -1,3 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.pl$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pm$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("cpanfile$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("Daikufile$" . cperl-mode))
