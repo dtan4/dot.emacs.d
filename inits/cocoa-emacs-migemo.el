cdb6f9dba3dd9b852b9a455c1c4e1528989ef8f5
Daisuke Fujita <dtanshi45@gmail.com>
2014-12-30 18:27:09 +0900

    Move helm-migemo settings

--- inits/cocoa-emacs-migemo.el
+++ inits/cocoa-emacs-migemo.el
@@ -9,4 +9,19 @@
   (setq helm-use-migemo t)
   (load-library "migemo")
   (migemo-init)
-)
+
+  ;; http://rubikitch.com/2014/12/19/helm-migemo/
+  (require 'helm-migemo)
+  (eval-after-load "helm-migemo"
+    '(defun helm-compile-source--candidates-in-buffer (source)
+       (helm-aif (assoc 'candidates-in-buffer source)
+           (append source
+                   `((candidates
+                      . ,(or (cdr it)
+                             (lambda ()
+                               ;; Do not use `source' because other plugins
+                               ;; (such as helm-migemo) may change it
+                               (helm-candidates-in-buffer (helm-get-current-source)))))
+                     (volatile) (match identity)))
+         source)))
+  )
