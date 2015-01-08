8912afff597dee600c8510f6ceef44f86b569c12
Daisuke Fujita <dtanshi45@gmail.com>
2015-01-08 14:54:58 +0900

    Configure IME for Emacs Mac Port

--- inits/cocoa-emacs-ime.el
+++ inits/cocoa-emacs-ime.el
@@ -1,9 +1,13 @@
-;; (14-11-07) inline-patch is not compatible with Emacs 24.4!!!!!
+;; https://github.com/railwaycat/emacs-mac-port/issues/62
+(when (equal system-type 'darwin)
+  (setq mac-option-modifier 'meta) ;; Bind meta to ALT
+  (setq mac-command-modifier 'super) ;; Bind apple/command to  super if you want
+  (setq mac-function-modifier 'hyper)) ;; Bind function key to hyper if you want
 
-;; (setq default-input-method "MacOSX")
-;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `title "あ")
-;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.base" `cursor-color "red")
-;; (mac-set-input-method-parameter "com.google.inputmethod.Japanese.Roman" `cursor-color "yellow")
-;; (add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
-;; (add-hook 'isearch-mode-hook 'mac-change-language-to-us)
-;; (mac-translate-from-yen-to-backslash)
+;; http://masutaka.net/chalow/2015-01-04-1.html
+(mac-auto-ascii-mode 1)
+(defun mac-selected-keyboard-input-source-change-hook-func ()
+  (set-cursor-color (if (string-match "\\.Roman$" (mac-input-source))
+                        "yellow" "red")))
+(add-hook 'mac-selected-keyboard-input-source-change-hook
+          'mac-selected-keyboard-input-source-change-hook-func)
