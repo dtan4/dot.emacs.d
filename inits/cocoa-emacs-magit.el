2511a8d32ee855958edd2e0ea6f4479bed6d0950
Daisuke Fujita <dtanshi45@gmail.com>
2015-01-08 14:56:21 +0900

    Configure magit for Emacs Mac Port

--- inits/cocoa-emacs-magit.el
+++ inits/cocoa-emacs-magit.el
@@ -1,1 +1,1 @@
-(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.4/bin/emacsclient")
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs-mac/emacs-24.4-mac-5.2/bin/emacsclient")
