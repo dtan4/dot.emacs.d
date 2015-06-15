5417d4e3dfbc67076dea3dc90d9467c87ba53dd8
Daisuke Fujita <dtanshi45@gmail.com>
2015-06-15 22:50:12 +0900

    Install replace-colorthemes

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
 (el-get-bundle color-moccur)
 (el-get-bundle cperl-mode)
 (el-get-bundle dockerfile-mode)
+(el-get-bundle emacs-jp/replace-colorthemes)
 ;; (el-get-bundle emacsmirror/yatex)
 (el-get-bundle enh-ruby-mode)
 (el-get-bundle exec-path-from-shell)
