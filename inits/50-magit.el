2ff7d293238650be57cd06efab634265af3bc7ea
Daisuke Fujita <dtanshi45@gmail.com>
2015-07-06 01:51:11 +0900

    Remove obsolete color settings of magit

--- inits/50-magit.el
+++ inits/50-magit.el
@@ -4,12 +4,6 @@
 (global-set-key (kbd "C-c b") 'magit-blame-mode)
 (global-set-key (kbd "C-c l") 'magit-file-log)
 
-(eval-after-load "magit"
-  '(progn
-     (set-face-foreground 'magit-diff-add "#b9ca4a")
-     (set-face-foreground 'magit-diff-del "#d54e53")
-     (set-face-background 'magit-item-highlight "#000000")))
-
 ;; magit 1.4.0 or above
 (setq magit-auto-revert-mode nil)
 
