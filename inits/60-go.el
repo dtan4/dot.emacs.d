6432cea99733a68573af9347bef812c68abc0847
Daisuke Fujita <dtanshi45@gmail.com>
2015-07-23 17:23:11 +0900

    Rebind M-. to gode-jump (in the same window)

--- inits/60-go.el
+++ inits/60-go.el
@@ -4,4 +4,4 @@
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "M-.") 'godef-jump-other-window)))
+                          (local-set-key (kbd "M-.") 'godef-jump)))
