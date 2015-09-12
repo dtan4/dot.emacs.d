be274ab9ebe7deff0879a3c4a4a9da73beb9a52f
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-12 18:21:26 +0900

    Change font size in OS X

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -5,7 +5,7 @@
 (if (find-font (font-spec :name "Ricty"))
     (progn (set-face-attribute 'default nil
                         :family "Ricty"
-                        :height 160)
+                        :height 140)
            (set-fontset-font nil 'japanese-jisx0208
                              (font-spec :family "Ricty" :registry "unicode-bmp"))))
 
