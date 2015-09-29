0e7704942793f693425c907dad064730336ff632
Daisuke Fujita <dtanshi45@gmail.com>
2015-09-29 10:12:14 +0900

    Set indent level 2 in sh-mode

--- inits/60-shell.el
+++ inits/60-shell.el
@@ -1,3 +1,6 @@
 (add-to-list 'auto-mode-alist '(".envrc$" . sh-mode))
 (add-to-list 'auto-mode-alist '(".env$" . sh-mode))
 (add-to-list 'auto-mode-alist '(".env.sample$" . sh-mode))
+
+(setq sh-basic-offset 2)
+(setq sh-indentation 2)
