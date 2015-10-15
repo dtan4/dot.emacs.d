6ec882ac21d23c6e00aa83045cb68c5231e6af63
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-15 12:12:13 +0900

    Start flycheck with go-mode

--- inits/60-go.el
+++ inits/60-go.el
@@ -7,3 +7,4 @@
                           (local-set-key (kbd "M-.") 'godef-jump)))
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook 'flycheck-mode)
