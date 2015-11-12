78cc1169c04f242e0801a8cad7f77852a21c6459
Daisuke Fujita <dtanshi45@gmail.com>
2015-11-12 13:05:46 +0800

    Reinstall js2-mode

--- inits/60-javascript.el
+++ inits/60-javascript.el
@@ -1,6 +1,6 @@
 ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 ;; ;; https://github.com/magnars/.emacs.d/blob/master/setup-js2-mode.el
-;; (setq-default js2-global-externs '("module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "$" "jQuery" "_" "Backbone" "dojo" "ko"))
+(setq-default js2-global-externs '("exports" "require"))
 
 (setq js-indent-level 2)
 
