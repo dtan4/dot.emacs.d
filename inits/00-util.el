52719a5d8fc9f5df89298ead64b9028323e4a2d7
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-26 17:58:54 +0900

    Open file with line number by C-x C-f

--- inits/00-util.el
+++ inits/00-util.el
@@ -129,3 +129,21 @@
  '(hl-line (
             (t
              (:background "gray10")))))
+
+;; Open file with line number
+;; http://stackoverflow.com/questions/3139970/open-a-file-at-line-with-filenameline-syntax/3141456#3141456
+(defadvice find-file (around find-file-line-number
+                             (filename &optional wildcards)
+                             activate)
+  "Turn files like file.cpp:14 into file.cpp and going to the 14-th line."
+  (save-match-data
+    (let* ((matched (string-match "^\\(.*\\):\\([0-9]+\\):?$" filename))
+           (line-number (and matched
+                             (match-string 2 filename)
+                             (string-to-number (match-string 2 filename))))
+           (filename (if matched (match-string 1 filename) filename)))
+      ad-do-it
+      (when line-number
+        ;; goto-line is for interactive use
+        (goto-char (point-min))
+        (forward-line (1- line-number))))))
