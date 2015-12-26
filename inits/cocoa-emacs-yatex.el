5b4174802af7925e05520055eb135b657e1bca63
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-27 04:35:21 +0900

    Use dvipdfmx

--- inits/cocoa-emacs-yatex.el
+++ inits/cocoa-emacs-yatex.el
@@ -1,8 +1,9 @@
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
 (setq auto-mode-alist
       (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
-(setq tex-command "platex"
-      dvi2-command "open -a TeXShop")
+(setq tex-command "platex")
+(setq dviprint-command-format "dvipdfmx %s")
+(setq dvi2-command "open -a TeXShop")
 (setq YaTeX-kanji-code 4)
 (add-hook 'yatex-mode-hook
           '(lambda ()
