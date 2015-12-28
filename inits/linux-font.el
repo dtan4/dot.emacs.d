f51c79f8dfdf04bfd56bfa922838a7197ce04d23
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-28 19:49:36 +0900

    Set font size 13 in Linux

--- inits/linux-font.el
+++ inits/linux-font.el
@@ -1,6 +1,6 @@
 (if (find-font (font-spec :name "Ricty"))
     (set-face-attribute 'default nil
                         :family "Ricty"
-                        :height 150))
+                        :height 130))
 ;; (set-fontset-font nil 'japanese-jisx0208
 ;;                   (font-spec :family "Ricty" :registry "unicode-bmp"))
