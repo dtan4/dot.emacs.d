9738eb421d25249532c8ec89ca51e4941bc94a49
Daisuke Fujita <dtanshi45@gmail.com>
2016-04-09 15:18:24 +0900

    Use magit v2.6.0

--- init.el
+++ init.el
@@ -63,7 +63,7 @@
 (el-get-bundle js2-mode)
 (el-get-bundle json-mode)
 (el-get-bundle less-css-mode)
-(el-get-bundle magit :branch "master")
+(el-get-bundle magit :type github :checkout "f9dda76193e8bc46c0d3f03493a3f616c5ead810")
 (el-get-bundle markdown-mode)
 (el-get-bundle migemo)
 (el-get-bundle mmm-mode)
