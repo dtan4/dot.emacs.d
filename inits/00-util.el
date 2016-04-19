d80fd65c643b481776e76b4913faad19b59705bf
Daisuke Fujita <dtanshi45@gmail.com>
2016-04-19 11:59:51 +0900

    Enable highlighting line at idle

--- inits/00-util.el
+++ inits/00-util.el
@@ -124,7 +124,14 @@
 (electric-pair-mode 1)
 
 ;; Enable highlighting current line
-(global-hl-line-mode)
+;; http://rubikitch.com/2015/05/14/global-hl-line-mode-timer/
+(require 'hl-line)
+(defun global-hl-line-timer-function ()
+  (global-hl-line-unhighlight-all)
+  (let ((global-hl-line-mode t))
+    (global-hl-line-highlight)))
+(setq global-hl-line-timer
+      (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
 (custom-set-faces
  '(hl-line (
             (t
