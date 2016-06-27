5437fb216a74018fd0afb3fa65a77c02c22d975a
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-27 15:35:56 +0900

    Bind M-, to go back to tag

--- inits/60-go.el
+++ inits/60-go.el
@@ -4,8 +4,8 @@
 
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "M-.") 'godef-jump)))
-(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)
+                          (local-set-key (kbd "M-,") 'pop-tag-mark)
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
 (add-hook 'go-mode-hook 'flycheck-mode)
 (add-hook 'go-mode-hook 'go-eldoc-setup)
