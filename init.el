8af5170d206af71bf2147fae8cf1d3c85eaf9f1d
Daisuke Fujita <dtanshi45@gmail.com>
2017-02-19 16:55:19 +0900

    Use magit 2.10.2

--- init.el
+++ init.el
@@ -68,7 +68,7 @@
 (el-get-bundle less-css-mode)
 (el-get-bundle magit/magit
   :type github
-  :checkout "2.10.0"
+  :checkout "2.10.2"
   :load-path "lisp/"
   :compile "lisp/"
   :build `(("make" ,(format "EMACSBIN=%s" el-get-emacs) "docs")
