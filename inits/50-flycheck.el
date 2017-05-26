ccdf4bb0e5e815ee16e286bb8be43c38a311f594
Daisuke Fujita <dtanshi45@gmail.com>
2017-05-26 14:22:30 +0900

    Define some keybinds for flycheck

--- inits/50-flycheck.el
+++ inits/50-flycheck.el
@@ -1,1 +1,5 @@
 (setq flycheck-check-syntax-automatically '(mode-enabled save))
+
+(global-set-key (kbd "C-c C-n") 'flycheck-next-error)
+(global-set-key (kbd "C-c C-p") 'flycheck-previous-error)
+(global-set-key (kbd "C-c C-l") 'flycheck-list-errors)
