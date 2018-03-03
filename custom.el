1a685827ad847688ba4bb64ebb65392b869a0c14
Daisuke Fujita <dtanshi45@gmail.com>
2018-03-03 19:16:12 +0900

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,12 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-search-threshold 1000)
+ '(git-gutter:update-interval 1)
+ '(help-at-pt-display-when-idle (quote (flymake-overlay)) nil (help-at-pt))
+ '(help-at-pt-timer-delay 0.1)
+ '(init-loader-show-log-after-init (quote error-only))
  '(package-selected-packages
    (quote
-    (zlc yasnippet yaml-mode web-mode volatile-highlights vimrc-mode undo-tree toml-mode terraform-mode smartrep slim-mode scss-mode sass-mode rust-mode ruby-end rspec-mode rhtml-mode powerline popwin php-mode nginx-mode multiple-cursors mozc-im mozc mmm-mode markdown-mode magit less-css-mode json-mode init-loader indent-guide htmlize highlight-symbol helm-projectile helm-ls-git helm-gtags helm-ghq helm-ag helm haml-mode groovy-mode go-eldoc gnuplot-mode git-gutter flymake-cursor flymake-coffee flycheck-gometalinter flycheck feature-mode expand-region exec-path-from-shell enh-ruby-mode dockerfile-mode company-go company color-moccur coffee-mode auto-highlight-symbol anzu ag))))
+    (zlc yasnippet yaml-mode web-mode volatile-highlights vimrc-mode undo-tree toml-mode terraform-mode smartrep slim-mode scss-mode sass-mode rust-mode ruby-end rspec-mode rhtml-mode powerline popwin php-mode nginx-mode multiple-cursors mozc-im mozc mmm-mode markdown-mode magit less-css-mode json-mode init-loader indent-guide htmlize highlight-symbol helm-projectile helm-ls-git helm-gtags helm-ghq helm-ag helm haml-mode groovy-mode go-eldoc gnuplot-mode git-gutter flymake-cursor flymake-coffee flycheck-gometalinter flycheck feature-mode expand-region exec-path-from-shell enh-ruby-mode dockerfile-mode company-go company color-moccur coffee-mode auto-highlight-symbol anzu ag)))
+ '(rspec-use-rake-flag nil)
+ '(terraform-indent-level 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:foreground "gray70"))))
+ '(font-lock-constant-face ((t (:foreground "orange"))))
+ '(font-lock-preprocessor-face ((t (:foreground "orange"))))
+ '(hl-line ((t (:background "gray10")))))
