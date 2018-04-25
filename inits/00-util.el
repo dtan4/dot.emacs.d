c5bc33d6e2afa8f340974fab25067ecccfe3af64
Daisuke Fujita <dtanshi45@gmail.com>
2018-04-25 15:18:36 +0900

    Remove triple-windows dividing feature

--- inits/00-util.el
+++ inits/00-util.el
@@ -67,31 +67,6 @@
 (setq vc-follow-symlinks t)
 (setq auto-revert-check-vc-info t)
 
-;; Divide into 3 windows
-;; http://d.hatena.ne.jp/yascentur/20110621/1308585547
-(defun split-window-vertically-n (num_wins)
-  (interactive "p")
-  (if (= num_wins 2)
-      (split-window-vertically)
-    (progn
-      (split-window-vertically
-       (- (window-height) (/ (window-height) num_wins)))
-      (split-window-vertically-n (- num_wins 1)))))
-(defun split-window-horizontally-n (num_wins)
-  (interactive "p")
-  (if (= num_wins 2)
-      (split-window-horizontally)
-    (progn
-      (split-window-horizontally
-       (- (window-width) (/ (window-width) num_wins)))
-      (split-window-horizontally-n (- num_wins 1)))))
-(global-set-key "\C-x@" '(lambda ()
-                           (interactive)
-                           (split-window-vertically-n 3)))
-(global-set-key "\C-x#" '(lambda ()
-                           (interactive)
-                           (split-window-horizontally-n 3)))
-
 ;; Enable electric-pair-mode
 (electric-pair-mode 1)
 
