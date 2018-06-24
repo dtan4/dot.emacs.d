8a3245f43377b2292e36a76a33ab6f7675f3129e
Daisuke Fujita <dtanshi45@gmail.com>
2018-06-24 17:35:19 +0900

    Use global-display-line-mode with Emacs 26

--- inits/40-display-line-numbers.el
+++ inits/40-display-line-numbers.el
@@ -1,8 +1,14 @@
-(require 'linum)
-(global-linum-mode t)
-(setq linum-format "%4d")
+;; Emacs 26 has global-display-line-numbers-mode, which is lighter than linum-mode
+;; https://www.emacswiki.org/emacs/LineNumbers
 
-;; http://d.hatena.ne.jp/daimatz/20120215/1329248780
-(setq linum-delay t)
-(defadvice linum-schedule (around my-linum-schedule () activate)
-  (run-with-idle-timer 0.2 nil #'linum-update-current))
+(if (version< emacs-version "26.0")
+    (progn (require 'linum)
+           (global-linum-mode t)
+           (setq linum-format "%4d")
+
+           ;; http://d.hatena.ne.jp/daimatz/20120215/1329248780
+           (setq linum-delay t)
+           (defadvice linum-schedule (around my-linum-schedule () activate)
+             (run-with-idle-timer 0.2 nil #'linum-update-current)))
+  (progn (global-display-line-numbers-mode t)
+         (setq display-line-numbers "%4d")))
