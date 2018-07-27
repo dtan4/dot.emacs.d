3c161f408061a2bfd7266e93112b7211df913255
Daisuke Fujita <dtanshi45@gmail.com>
2018-07-27 16:09:56 +0900

    Assign markdown-table-align to C-<tab>

--- inits/60-markdown.el
+++ inits/60-markdown.el
@@ -4,5 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.apib$" . gfm-mode))
 
+(define-key markdown-mode-map (kbd "C-<tab>") 'markdown-table-align)
+
 (setq markdown-indent-on-enter nil)
 (setq markdown-gfm-use-electric-backquote nil)
