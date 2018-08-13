7b317ec7651596b428623c425cfc9a3186e9f4c3
Daisuke Fujita <dtanshi45@gmail.com>
2018-08-13 12:34:48 +0900

    Remove markdown-mode key map

--- inits/60-markdown.el
+++ inits/60-markdown.el
@@ -4,7 +4,5 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.apib$" . gfm-mode))
 
-(define-key markdown-mode-map (kbd "C-<tab>") 'markdown-table-align)
-
 (setq markdown-indent-on-enter nil)
 (setq markdown-gfm-use-electric-backquote nil)
