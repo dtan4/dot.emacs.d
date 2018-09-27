e8596dd9d809d22e532667382b4d362ab7e31986
Daisuke Fujita <dtanshi45@gmail.com>
2018-09-27 21:23:14 +0900

    Declare projectile-command-map

--- inits/50-projectile.el
+++ inits/50-projectile.el
@@ -1,1 +1,4 @@
+;; https://github.com/bbatsov/projectile/blob/4cd984edb0f7783002c33f0ee6938d9118382656/doc/installation.md
+
 (projectile-global-mode)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
