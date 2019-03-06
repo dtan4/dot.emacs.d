776bf71867d5ddc20ebf99f6d164165c77b692f5
Daisuke Fujita <dtanshi45@gmail.com>
2019-03-07 01:13:12 +0900

    Delete unused foreign-regexp and revive configurations

--- inits/40-foreign-regexp.el
+++
@@ -1,8 +0,0 @@
-;; (require 'foreign-regexp)
-;; (custom-set-variables
-;;  '(foreign-regexp/regexp-type 'ruby)
-;;  '(reb-re-syntax 'foreign-regexp))
-
-;; M-s M-s: incremental regexp search
-;; M-s M-r: incremental regexp search (reverse)
-;; M-s M-%: regexp replace
