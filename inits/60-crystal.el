b9e8c151a9ed6a772c69773cb16274a43e737342
Daisuke Fujita <dtanshi45@gmail.com>
2019-03-10 23:34:52 +0900

    Delete crystal configuration

--- inits/60-crystal.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'auto-mode-alist '("Projectfile$" . crystal-mode))
-
-(add-hook 'crystal-mode-hook 'ruby-end-mode)
-(add-hook 'crystal-mode-hook 'electric-pair-mode)
-(add-hook 'crystal-mode-hook 'electric-indent-mode)
-(add-hook 'crystal-mode-hook 'electric-layout-mode)
