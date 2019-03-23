35d5a5b92537a69033f238d4b5b9db0efbe87545
Daisuke Fujita <dtanshi45@gmail.com>
2019-03-23 18:35:49 +0900

    Uninstall flycheck-gometalinter
    gometalinter is deprecated

--- inits/60-go.el
+++ inits/60-go.el
@@ -2,15 +2,4 @@
   :ensure t
   :config
   (add-hook 'before-save-hook 'gofmt-before-save)
-  (setenv "GO111MODULE" "on")
-
-  (use-package flycheck-gometalinter
-    :ensure t
-    :config
-    (add-hook 'go-mode-hook 'flycheck-mode)
-    (eval-after-load 'flycheck
-      '(add-hook 'flycheck-mode-hook #'flycheck-gometalinter-setup))
-    (setq flycheck-gometalinter-vendor t)
-    (setq flycheck-gometalinter-disable-all t)
-    (setq flycheck-gometalinter-enable-linters '("golint" "varcheck" "vet"))
-    (setq flycheck-gometalinter-deadline "30s")))
+  (setenv "GO111MODULE" "on"))
