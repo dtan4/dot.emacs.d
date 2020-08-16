b3e2580b0fff415651e054bf2d2d4a2cee316a6c
Daisuke Fujita <dtanshi45@gmail.com>
2020-08-17 02:20:13 +0900

    Configure fonts only in GUI mode

--- inits/cocoa-emacs-appearance.el
+++ inits/cocoa-emacs-appearance.el
@@ -2,21 +2,23 @@
 ;; 01234567890123456789
 ;; あいうえおかきくけこ
 ;; 开发者中心
-(if (find-font (font-spec :name "Ricty"))
-    (progn (set-face-attribute 'default nil
-                        :family "Ricty"
-                        :height 120)
-           (set-fontset-font nil 'japanese-jisx0208
-                             (font-spec :family "Ricty" :registry "unicode-bmp"))))
+(if (display-graphic-p)
+    (if (find-font (font-spec :name "Ricty"))
+        (progn (set-face-attribute 'default nil
+                                   :family "Ricty"
+                                   :height 120)
+               (set-fontset-font nil 'japanese-jisx0208
+                                 (font-spec :family "Ricty" :registry "unicode-bmp"))))
 
-(set-fontset-font t 'han (font-spec :family "Hei" :height 160))
+  (set-fontset-font t 'han (font-spec :family "Hei" :height 160))
 
-;; Required to show green characters
-(setq default-frame-alist initial-frame-alist)
+  ;; Required to show green characters
+  (setq default-frame-alist initial-frame-alist)
 
-(set-scroll-bar-mode nil)
+  (set-scroll-bar-mode nil)
 
-;; http://d.hatena.ne.jp/rkworks/20130124/1359042654
-(set-frame-parameter nil 'alpha 90)
+  ;; http://d.hatena.ne.jp/rkworks/20130124/1359042654
+  (set-frame-parameter nil 'alpha 90)
 
-;; (toggle-frame-fullscreen)
+  ;; (toggle-frame-fullscreen)
+  )
--- inits/linux-font.el
+++ inits/linux-font.el
@@ -1,6 +1,8 @@
-(if (find-font (font-spec :name "Ricty"))
-    (set-face-attribute 'default nil
-                        :family "Ricty"
-                        :height 120))
-;; (set-fontset-font nil 'japanese-jisx0208
-;;                   (font-spec :family "Ricty" :registry "unicode-bmp"))
+(if (display-graphic-p)
+    (if (find-font (font-spec :name "Ricty"))
+        (set-face-attribute 'default nil
+                            :family "Ricty"
+                            :height 120))
+  ;; (set-fontset-font nil 'japanese-jisx0208
+  ;;                   (font-spec :family "Ricty" :registry "unicode-bmp"))
+  )
