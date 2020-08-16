7360175479380dad36b21ccfcb0fb99f13a610bb
Daisuke Fujita <dtanshi45@gmail.com>
2020-08-17 01:17:48 +0900

    Delete font related configurations

--- inits/linux-font.el
+++
@@ -1,6 +0,0 @@
-(if (find-font (font-spec :name "Ricty"))
-    (set-face-attribute 'default nil
-                        :family "Ricty"
-                        :height 120))
-;; (set-fontset-font nil 'japanese-jisx0208
-;;                   (font-spec :family "Ricty" :registry "unicode-bmp"))
