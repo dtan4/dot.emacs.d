27eaa78dbb984bff4b930939e0ed4f510627096d
Daisuke Fujita <dtanshi45@gmail.com>
2020-09-13 18:52:31 +0900

    Change font size in Windows & X410

--- inits/linux-font.el
+++ inits/linux-font.el
@@ -2,6 +2,6 @@
     (if (find-font (font-spec :name "Ricty Diminished"))
         (set-face-attribute 'default nil
                             :family "Ricty Diminished"
-                            :height 120))
+                            :height 105))
   (set-fontset-font nil 'japanese-jisx0208
-                    (font-spec :family "Ricty Diminished" :height 120)))
+                    (font-spec :family "Ricty Diminished" :height 105)))
