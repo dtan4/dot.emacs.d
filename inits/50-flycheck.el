665c52d6f5512bffa7619380bd2fe9bbf7dda015
Daisuke Fujita <dtanshi45@gmail.com>
2020-10-13 17:48:56 +0900

    Enable Flycheck with sh-mode for ShellCheck

--- inits/50-flycheck.el
+++ inits/50-flycheck.el
@@ -2,5 +2,7 @@
   :bind (("C-c C-n" . flycheck-next-error)
          ("C-c C-p" . flycheck-previous-error)
          ("C-c C-l" . flycheck-list-errors))
+  :hook
+  (sh-mode . flycheck-mode)
   :config
   (setq flycheck-check-syntax-automatically '(mode-enabled save)))
