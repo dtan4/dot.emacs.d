579f1b31e748b45576eae28f2ec32b7252daa87c
Daisuke Fujita <dtanshi45@gmail.com>
2021-02-16 22:29:15 +0900

    Adjust mode-line colors

--- inits/40-color-theme.el
+++ inits/40-color-theme.el
@@ -2,4 +2,9 @@
     (use-package color-theme-modern
       :config
       (load-theme 'dark-laptop t t)
-      (enable-theme 'dark-laptop)))
+      (enable-theme 'dark-laptop)
+      (set-face-foreground 'mode-line "white")
+      (set-face-background 'mode-line "gray15")
+      (set-face-background 'mode-line-inactive "black")
+      (set-face-foreground 'mode-line-buffer-id "white")
+      (set-face-background 'mode-line-buffer-id "black")))
